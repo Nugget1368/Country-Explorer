b12280a65e7c1ef16ac1a095bc4f91c45805caee
Nugget1368 <rneg1@hotmail.com>
2025-09-16 09:47:52 +0200

    removed comments

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import styles from './CountryList.module.css'
 
 const CountryList = ({countries}) => {
-    // let { countries } = useSelector((state) => state.region)
     return (
         <ul>
             {
