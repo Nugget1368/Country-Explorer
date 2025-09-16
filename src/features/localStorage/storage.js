8ef89d818ef98e3f0f736e9fabd5569d02152ea5
Nugget1368 <rneg1@hotmail.com>
2025-09-16 11:33:23 +0200

    Save Countries to Sessionstorage Updated LocalStorage name to Storage.js

--- src/features/localStorage/storage.js
+++ src/features/localStorage/storage.js
@@ -1,15 +1,28 @@
 const SAVED_COUNTRIES_KEY = "savedCountries";
 
-export class MyLocalStorage { 
+export class MyLocalStorage {
     static getSavedCountries = () => {
         let savedCountries = JSON.parse(localStorage.getItem(SAVED_COUNTRIES_KEY));
         return savedCountries || [];
     };
-    
+
     static saveCountry = (country) => {
         let savedCountries = this.getSavedCountries();
         savedCountries.push(country);
         localStorage.setItem(SAVED_COUNTRIES_KEY, JSON.stringify(savedCountries));
         return true;
+    };
+}
+
+export class MySessionStorage {
+    static getRegion = (regionName) => {
+        let region = JSON.parse(sessionStorage.getItem(regionName));
+        return region || [];
+    };
+
+    static saveRegion = (regionName, countries) => {
+        if (!sessionStorage.getItem(regionName)) {
+            sessionStorage.setItem(regionName, JSON.stringify(countries));
+        }
     };
 }
--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -1,19 +1,28 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchRegion } from '../features/region/regionSlice.js'
 import CountryList from '../components/CountryList'
+import { MySessionStorage } from '../features/localStorage/storage.js'
+import { setCountries } from '../features/region/regionSlice.js'
+import { useEffect, useState } from 'react'
 
 
 const Countries = () => {
     const { countries, status, error } = useSelector(state => state.region);
     const dispatch = useDispatch();
     const regions = ['europe', 'africa', 'asia', 'america', 'oceania'];
-    
-    const handleClick = async (region) => {
-        if(!countries.some(country => country.region === region))
-        {
-            dispatch(fetchRegion(region))
-        }
+    const [region, setRegion] = useState("");
+
+    const handleClick = async (_region) => {
+        setRegion(_region);
+        //Get countries from Sessionstorage
+        const localCountries = MySessionStorage.getRegion(_region);
+        //Fetch Countries from API || Set countries from Sessionstorage
+        localCountries.length === 0 ? dispatch(fetchRegion(_region)) : dispatch(setCountries(localCountries));
     }
+    
+    useEffect(() => {
+        MySessionStorage.saveRegion(region, countries);
+    }, [countries]);
 
     return (
         <>
@@ -23,14 +32,14 @@ const Countries = () => {
                 </header>
                 <article>
                     <div>
-                        {regions.map(region => <button key={region} onClick={() => handleClick(region)} className='btn'>{region.charAt(0).toUpperCase() + region.slice(1)}</button>)}
+                        {regions.map(r => <button key={r} onClick={() => handleClick(r)} className='btn'>{r.charAt(0).toUpperCase() + r.slice(1)}</button>)}
                     </div>
                 </article>
             </section>
             <section>
-                {status === "loading" ? <h3>Loading...</h3> : 
-                status === "succeeded" ? <CountryList countries={countries} /> :
-                status === "failed" ? <h3>{error}</h3> : null
+                {status === "loading" ? <h3>Loading...</h3> :
+                    status === "succeeded" ? <CountryList countries={countries} /> :
+                        status === "failed" ? <h3>{error}</h3> : null
                 }
             </section>
         </>
