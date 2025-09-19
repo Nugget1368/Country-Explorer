88e8ad1da548cda660d4305abda0239a98ec31f9
Nugget1368 <rneg1@hotmail.com>
2025-09-19 09:54:31 +0200

    Reset Quizstatus to idle onMounting

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -5,9 +5,16 @@ import QuizResult from "../components/quiz/QuizResult";
 import LeaderboardList from "../components/quiz/leaderboardList";
 import style from "./quiz.module.css"
 import ReturnBtn from "../components/buttons/ReturnBtn";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setQuizStatusIdle } from "../features/region/regionSlice";
 const Quiz = () => {
     const { quizStatus } = useSelector(state => state.region);
     const { status } = useSelector(state => state.region);
+    let dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(setQuizStatusIdle())
+    }, []);
 
     return (
         <section className={style.article}>
